027aee57f609b4df025bf9d237da41a675c464a7
SiHan-Son <sihan830@gmail.com>
2018-09-15 11:07:23 +0900

    [ADD] img_open 예제

---
+++ codes/01. img_open.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+
+def showImage():
+    fileName = "../images/lena.jpg"
+    img = cv2.imread(fileName, cv2.IMREAD_COLOR)
+
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+showImage()
